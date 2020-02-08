929938f215da80b021d07cf0c724ca14d41d0305
wangzhen0101 <wzworkmail@163.com>
2020-02-08 16:33:37 +0800

    Modify log path

--- test.go
+++ test.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"github.com/wangzhen0101/wzbtc/peer"
-	"time"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	initLogRotator("F:\\goproject\\src\\github.com\\wangzhen0101\\wzbtc\\wzbtc-log")
+	logPath, _ := os.Getwd()
+	logPath = filepath.Join(logPath, "wzbtc-log")
+	initLogRotator(logPath)
 	setLogLevels("trace")
-	for {
-		netLog.Tracef("test netlog, %d", 11)
-		netLog.Debugf("test netlog, %d", 11)
-		netLog.Infof("test netlog, %d", 11)
-		netLog.Warnf("test netlog, %d", 11)
-		netLog.Errorf("test netlog, %d", 11)
-		netLog.Fatalf("test netlog, %d", 11)
-		peer.TestNetLog("call by main")
-		time.Sleep(1000)
-	}
 
+	netLog.Tracef("test netlog, %d", 11)
+	netLog.Debugf("test netlog, %d", 11)
+	netLog.Infof("test netlog, %d", 11)
+	netLog.Warnf("test netlog, %d", 11)
+	netLog.Errorf("test netlog, %d", 11)
+	netLog.Fatalf("test netlog, %d", 11)
+	peer.TestNetLog("call by main")
 }
